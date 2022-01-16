175348a77279cee33996e69ec3b31201529d28e4
Evgeny.Shinkarev@urfu.me <Evgeny.Shinkarev@urfu.me>
2022-01-16 15:59:28 +0500

    ещё попытка

--- src/back.ts
+++ src/back.ts
@@ -3,4 +3,5 @@ const app = express();
 app.get("/", (res, req) => {
     req.status(200).json("server working");
 })
-app.listen(5000, () => console.log("started"));
+const port = process.env.port || 5050;
+app.listen(port, () => console.log(`server started at port ${port}`));
